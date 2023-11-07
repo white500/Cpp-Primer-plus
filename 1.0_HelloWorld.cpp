fcf48bfd8c45af9f5221e079283385d9a7f07023
white500 <964142664@qq.com>
2023-11-07 19:34:23 +0800

    首次上传

---
+++ 1.0_HelloWorld.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using std::cin;
+using std::cout;
+using std::endl;
+
+int main(void)
+{
+    cout << "Come up and C++ me some time.";
+    cout << endl;
+    cout << "new line1";
+    return 0;
+}
---
+++ 2.4_sqrt.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <cmath>
+
+// sqrt.cpp -- using the sqrt() function
+int main () 
+{
+    using namespace std;
+
+    double area;
+    cout << "Enter the floor area, in square feet, of your home: ";
+    cin >> area;
+    double side;
+    side = sqrt(area);
+    cout << "That's the equivalent of a square " << side
+         << " feet to the side." << endl;
+    cout << "How fascinating!" << endl;
+
+    return 0;
+}
